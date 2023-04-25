45f52ad70d508da0dd4e64f4c0bdaece3f21215e
felixmurcia <felixmurcia@gmail.com>
2023-04-25 09:50:17 +0200

    Optimize imports

--- src/main/java/virtualcave/springbootexercice1/exercise/dto/FilterDto.java
+++ src/main/java/virtualcave/springbootexercice1/exercise/dto/FilterDto.java
@@ -1,6 +1,11 @@
 package virtualcave.springbootexercice1.exercise.dto;
 
-import lombok.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
 import org.springframework.stereotype.Component;
 
 import java.time.LocalDate;
